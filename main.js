66b0f20989c979949fbc336b222f68682a002001
MrPig91 <sirius.cleveland@gmail.com>
2021-09-19 19:13:06 -0500

    added hover js script to start gif

---
+++ main.js
@@ -0,0 +1,25 @@
+pwsh_img = document.getElementById("pwsh-img")
+arduino_img = document.getElementById("arduino-img")
+
+pwsh_img.addEventListener("mouseover", function () {
+    mouseOver(this,"images\\Win.gif")
+})
+pwsh_img.addEventListener("mouseout", function () {
+    mouseOut(this,"images\\Win.jpg")
+})
+
+arduino_img.addEventListener("mouseover", function () {
+    console.log("hello")
+    mouseOver(this,"images\\ttt-sample.gif")
+})
+arduino_img.addEventListener("mouseout", function () {
+    mouseOut(this,"images\\ttt-sample.jpg")
+})
+
+function mouseOver(event,gif) {
+    event.src = gif
+}
+
+function mouseOut(event,jpg) {
+    event.src = jpg
+}
